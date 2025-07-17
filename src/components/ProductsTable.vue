c5cc873368daee8411004324b32033835aa6cde1
Kentillation <kentanthony070495@gmail.com>
2025-07-17 22:07:07 +0800

    Deployment

--- src/components/ProductsTable.vue
+++ src/components/ProductsTable.vue
@@ -58,7 +58,7 @@
 
         <!--eslint-disable-next-line -->
         <template v-slot:item.availability_label="{ item }">
-            <v-chip :color="item.availability_id === 1 ? 'green' : 'red'" size="small" variant="flat">
+            <v-chip :color="item.availability_id === 1 ? 'red' : 'green'" size="small" variant="flat">
                 {{ item.availability_label }}
             </v-chip>
         </template>
