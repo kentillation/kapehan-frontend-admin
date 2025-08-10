32a49a9deeb95b3b39327f7aea696448f6fd03d0
Kent Anthony <126381769+kentillation@users.noreply.github.com>
2025-08-10 22:47:35 +0800

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -9,4 +9,13 @@ const apiClient = axios.create({
     withCredentials: process.env.VUE_APP_WITH_CREDENTIALS
 });
 
+// Add interceptor
+apiClient.interceptors.request.use(config => {
+  const token = localStorage.getItem('auth_token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 export default apiClient;
